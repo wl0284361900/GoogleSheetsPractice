417cec313fd8c97247fb5f2c83d26019c0000337
詹竣亦 <B10517044@gemail.yuntech.edu.tw>
2020-03-25 20:38:09 +0800

    GET 成功

--- GoogleSheetsPractice/ViewController.m
+++ GoogleSheetsPractice/ViewController.m
@@ -21,6 +21,7 @@
     //1. 首先構造一個NSURL請求資源地址
     // 構造URL資源地址
     NSURL *url = [NSURL URLWithString:@"https://sheetsu.com/apis/v1.0bu/d7db3e5c2a05"];
+    
     //2. 建立一個NSRequest請求物件
     // 建立Request請求
     NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
@@ -35,36 +36,83 @@
     [request setCachePolicy:NSURLRequestReloadIgnoringLocalCacheData];
     
     /*
-    NSURLRequestUseProtocolCachePolicy = 0 //預設的快取策略， 如果快取不存在，直接從服務端獲取。如果快取存在，會根據response中的Cache-Control欄位判斷下一步操作，如: Cache-Control欄位為must-revalidata, 則詢問服務端該資料是否有更新，無更新的話直接返回給使用者快取資料，若已更新，則請求服務端.
-    NSURLRequestReloadIgnoringLocalCacheData = 1 //忽略本地快取資料，直接請求服務端.
-    NSURLRequestIgnoringLocalAndRemoteCacheData = 4 //忽略本地快取，代理伺服器以及其他中介，直接請求源服務端.
-    NSURLRequestReloadIgnoringCacheData = NSURLRequestReloadIgnoringLocalCacheData
-    NSURLRequestReturnCacheDataElseLoad = 2 //有快取就使用，不管其有效性(即忽略Cache-Control欄位), 無則請求服務端.
-     NSURLRequestReturnCacheDataDontLoad = 3 //只載入本地快取. 沒有就失敗. (確定當前無網路時使用)
-    NSURLRequestReloadRevalidatingCacheData = 5 //快取資料必須得得到服務端確認有效才使用
-    */
+     NSURLRequestUseProtocolCachePolicy = 0
+     //預設的快取策略，如果快取不存在，直接從服務端獲取。
+     如果快取存在，會根據response中的Cache-Control欄位判斷下一步操作，
+     如: Cache-Control欄位為must-revalidata,
+     則詢問服務端該資料是否有更新，無更新的話直接返回給使用者快取資料，若已更新，則請求服務端.
+     
+     NSURLRequestReloadIgnoringLocalCacheData = 1
+     //忽略本地快取資料，直接請求服務端.
+     
+     NSURLRequestIgnoringLocalAndRemoteCacheData = 4
+     //忽略本地快取，代理伺服器以及其他中介，直接請求源服務端.
+     NSURLRequestReloadIgnoringCacheData = NSURLRequestReloadIgnoringLocalCacheData
+     
+     NSURLRequestReturnCacheDataElseLoad = 2
+     //有快取就使用，不管其有效性(即忽略Cache-Control欄位), 無則請求服務端.
+     
+     NSURLRequestReturnCacheDataDontLoad = 3
+     //只載入本地快取. 沒有就失敗. (確定當前無網路時使用)
+     
+     NSURLRequestReloadRevalidatingCacheData = 5
+     //快取資料必須得得到服務端確認有效才使用
+     */
     
     //3. 建立NSURLSession會話物件
     // 採用蘋果提供的共享session
     NSURLSession *sharedSession = [NSURLSession sharedSession];
     // 構造NSURLSessionConfiguration
     NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    
+    //也可以通過NSURLSessionConfiguration統一設定超時時間、請求頭等資訊
+    //設定請求超時為10秒鐘
+    configuration.timeoutIntervalForRequest = 10;
+    //在蜂窩網路情況下是否繼續請求（上傳或下載）
+    configuration.allowsCellularAccess = NO;
+    //配置請求頭
+    configuration.HTTPAdditionalHeaders = @{@"Content-Encoding":@"gzip"};
     // 構造NSURLSession，網路會話；
     NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration];
     
     /*
      通過NSURLSessionConfiguration提供了三種建立NSURLSession的方式
      //預設配置使用的是持久化的硬碟快取，儲存證書到使用者鑰匙鏈。儲存cookie到shareCookie。
+     
      defaultSessionConfiguration
      //不使用永久持存cookie、證書、快取的配置，最佳優化資料傳輸。
+     
      ephemeralSessionConfiguration
      //可以上傳下載HTTP和HTTPS的後臺任務(程式在後臺執行)。
+     
      backgroundSessionConfigurationWithIdentifier
 
      在後臺時，將網路傳輸交給系統的單獨的一個程序,即使app掛起、推出甚至崩潰照樣在後臺執行。
-
-     也可以通過NSURLSessionConfiguration統一設定超時時間、請求頭等資訊
      */
+    
+    //建立NSURLSessionTask物件，然後執行
+    // 構造NSURLSessionTask，會話任務；
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        if(error){
+            NSLog(@"get error: %@", error.localizedDescription);
+        }else{
+            //請求成功，解析資料
+            // JSON資料格式解析
+            id object = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableLeaves error:&error];
+            //判斷是否解析成功
+            if(error){
+                NSLog(@"get error: %@", error.localizedDescription);
+            }else{
+                // 解析成功，處理資料，通過GCD獲取主佇列，在主執行緒中重新整理介面。
+                NSLog(@"get success: %@", object);
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    // 重新整理介面....
+                });
+            }
+        }
+    }];
+    [task resume];
+    
 }
 
 
